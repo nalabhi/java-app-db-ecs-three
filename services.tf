b2b815a5d3af0c77e799240531c92a3ca9abc0e0
Abhijit Shinde <a.shinde@globant.com>
2022-05-16 18:35:30 +0530

    first commit

--- services.tf
+++ services.tf
@@ -22,22 +22,22 @@
 #    cross_zone_load_balancing = true
 #}
 
-resource "aws_ecs_task_definition" "test-http" {
-    family = "test-http"
-    container_definitions = "${file("task-definitions/test-http.json")}"
-}
+#resource "aws_ecs_task_definition" "test-http" {
+#    family = "test-http"
+#    container_definitions = "${file("task-definitions/test-http.json")}"
+#}
 
-resource "aws_ecs_service" "test-http" {
-    name = "test-http"
-    cluster = "${aws_ecs_cluster.main.id}"
-    task_definition = "${aws_ecs_task_definition.test-http.arn}"
-    iam_role = "${aws_iam_role.ecs_service_role.arn}"
-    desired_count = 1
-    depends_on = ["aws_iam_role_policy.ecs_service_role_policy"]
+#resource "aws_ecs_service" "test-http" {
+#    name = "test-http"
+#    cluster = "${aws_ecs_cluster.main.id}"
+#    task_definition = "${aws_ecs_task_definition.test-http.arn}"
+#    iam_role = "${aws_iam_role.ecs_service_role.arn}"
+#    desired_count = 1
+#    depends_on = ["aws_iam_role_policy.ecs_service_role_policy"]
 
     #load_balancer {
     #    elb_name = "${aws_elb.test-http.id}"
     #    container_name = "test-http"
     #    container_port = 8080
     #}
-}
+#}
