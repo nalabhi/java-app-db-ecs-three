37c3b8d4fcab144cf5686763e6e1975e821559a8
Abhijit Shinde <a.shinde@globant.com>
2022-05-16 17:30:55 +0530

    first commit

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "amis" {
     description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
     # TODO: support other regions.
     default = {
-        us-east-1 = "ami-ddc7b6b7"
+        us-east-1 = "ami-0f260fe26c2826a3d"
     }
 }
 
