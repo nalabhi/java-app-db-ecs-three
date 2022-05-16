364babb2fe91913b3f95cba1619d9916c32917a8
nalabhi <100408534+nalabhi@users.noreply.github.com>
2022-05-16 13:38:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_launch_configuration" "ecs" {
     security_groups = ["${aws_security_group.ecs.id}"]
     iam_instance_profile = "${aws_iam_instance_profile.ecs.name}"
     # TODO: is there a good way to make the key configurable sanely?
-    key_name = "${aws_key_pair.abc.key_name}"
+    #key_name = "${aws_key_pair.abc.key_name}"
     associate_public_ip_address = true
     user_data = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}' > /etc/ecs/ecs.config"
 }
